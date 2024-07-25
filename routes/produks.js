ce2985f560eb695031f14fdca17115dfd9b5e51c
unknown <2200016073@webmail.uad.ac.id>
2024-07-25 20:32:03 +0700

    haay

--- routes/produks.js
+++ routes/produks.js
@@ -5,7 +5,7 @@ const produkController = require('../controllers/produkController');
 const { authenticateToken } = require('../middleware/auth');
 
 
-router.get('/',authenticateToken(['admin', 'user']), produkController.getAllProduk);
+router.get('/', produkController.getAllProduk);
 router.get('/:id',produkController.getProdukById);
 router.post('/',authenticateToken(['admin']), produkController.createProduk);
 router.put('/:id', authenticateToken(['admin']), produkController.updateProduk);
